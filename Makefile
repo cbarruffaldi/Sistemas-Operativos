bd2bcb33a0a1aa3c42e578388d4a982831066ba2
tcerdaITBA <tcerda@itba.edu.ar>
2016-09-23 22:22:54 -0300

    server usa threads

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ SERVER=server.c
 
 sockets:
 	gcc $(FLAGS) $(CLIENT) sockets_IPC.c -o client.bin
-	gcc $(FLAGS) $(SERVER) sockets_IPC.c -o server.bin
+	gcc $(FLAGS) $(SERVER) sockets_IPC.c -l pthread -o server.bin
 
 fifo:
 	rm -f -v fifo_server fifo_peer_*
 	gcc $(FLAGS) $(CLIENT) fifo_IPC.c -o client.bin
-	gcc $(FLAGS) $(SERVER) fifo_IPC.c -o server.bin
+	gcc $(FLAGS) $(SERVER) fifo_IPC.c -l pthread -o server.bin
 
 clean:
 	rm -v *.bin
-	rm -f -v fifo_server fifo-peer-*
+	rm -f -v fifo_server /tmp/fifo_peer_*
--- server.c
+++ server.c
@@ -3,22 +3,32 @@
 #define SHUTDOWN "shutdown" // Si recibe "shutdown" se apaga el server
 #define ARG_COUNT 2
 
+#include <stdlib.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <signal.h>
 
-int attend(t_connectionADT con);
+typedef struct {
+  int id;
+  t_connectionADT con;
+} pthread_data;
+
+void * attend(void * con);
+int create_thread(int id, t_connectionADT con);
 
 int main(int argc, char *argv[])
 {
+  t_connectionADT con;
+  t_addressADT sv_addr;
+  int rc, id = 0;
+
   if(argc != ARG_COUNT) {
     fprintf(stderr, "Usage: %s <server_path>", argv[0]);
   return 1;
   }
-
-  t_connectionADT con;
-  t_addressADT sv_addr;
 
   printf("Opening channel...\n");
   sv_addr = create_address(argv[1]);
@@ -48,41 +58,60 @@ int main(int argc, char *argv[])
 
     printf("Accepted!\n");
 
-    if (fork() == 0)
-      return attend(con);
+    rc = create_thread(id++, con);
+
+    if (rc) {
+		  printf("Failed to create thread\n");
+		  return 1;
+    }
   }
 }
 
-int attend(t_connectionADT con) {
-    char msg[BUFSIZE];
-    int pid = getpid();
-    t_requestADT req;
-    t_response res = {.msg = "HEY!"};
+int create_thread(int id, t_connectionADT con) {
+  pthread_t thread;
+  pthread_data * thdata = malloc(sizeof(*thdata));
+  thdata->id = id;
+  thdata->con = con;
+  return pthread_create(&thread, NULL, attend, thdata);
+}
 
-    while(1) {
+void * attend(void * p) {
+	pthread_data *data = (pthread_data *) p;
+  char msg[BUFSIZE];
+  int id = data->id;
+	t_connectionADT con = data->con;
+  t_requestADT req;
+  t_response res = {.msg = "HEY!"};
 
-      printf("Reading request...\n");
+	// ya se copiaron los datos en id y en con; se libera data
+	free(data);
 
-      if ((req = read_request(con)) == NULL)  {
-        fprintf(stderr, "error reading request\n");
-        return 1;
-      }
+  printf("thread con: %p\nthread id: %d\n", con, id);
 
-      get_request_msg(req, msg);      // Copia el mensaje de req en msg
-      printf("Request read by server\n");
-      printf("Server pid: %d\nmsg: %s\n", pid, msg);
+  while(1) {
 
-      if (send_response(req, res) < 0) {   // Responde a cliente
-        fprintf(stderr, "error sending response\n");
-        return 1;
-      }
+    printf("Reading request...\n");
 
-      if (strcmp(SHUTDOWN, msg) == 0) {
-        printf("Shutting down...\n");
-        disconnect(con);
-        return 0;
-      }
+    if ((req = read_request(con)) == NULL)  {
+      fprintf(stderr, "error reading request\n");
+      pthread_exit(NULL);
     }
 
-    return 1;
+    get_request_msg(req, msg);      // Copia el mensaje de req en msg
+    printf("Request read by thread\n");
+    printf("Thread id: %d\nMsg: %s\n", id, msg);
+
+    if (send_response(req, res) < 0) {   // Responde a cliente
+      fprintf(stderr, "error sending response\n");
+      pthread_exit(NULL);
+    }
+
+    if (strcmp(SHUTDOWN, msg) == 0) {
+      printf("Shutting down...\n");
+      disconnect(con);
+      pthread_exit(NULL);
+    }
+  }
+
+  pthread_exit(NULL);
 }
