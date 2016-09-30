a381dae94fa7cef3c4b7b25ac0b8b60933cac2e3
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2016-09-30 15:33:28 -0300

    Log, falta ver lo de los niveles

--- server.c
+++ server.c
@@ -2,6 +2,7 @@
 #include "include/IPC.h"
 #include "include/server.h"
 #include "include/query.h"
+#include "include/log.h"
 
 #include <string.h>
 #include <unistd.h>
@@ -9,10 +10,18 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
 #define ARG_COUNT 3
 #define DATABASE_PROCESS "database.bin"
 #define PATH_SIZE 64
 
+typedef struct {
+  long mtype;
+  char mtext[BUFSIZE];
+}queue_buffer;
 
 typedef struct {
   char db_path[PATH_SIZE];
@@ -31,11 +40,15 @@ void send_query(t_session_data * data, const char *sql, char result[]);
 void * run_thread(void * p);
 void print_session_data(t_session_data *data);
 int create_thread(char * db_path, t_sessionADT session);
+int send_mq(char * msg, int priority);
+
+int msq_id; //id de la MQ
 
 int main(int argc, char *argv[])
 {
   t_master_sessionADT master_session;
   t_sessionADT session;
+  key_t key; //Key de la MQ
 
   if (argc != ARG_COUNT) {
     printf("Usage: %s <server_path> <database_path>\n", argv[0]);
@@ -49,10 +62,23 @@ int main(int argc, char *argv[])
 
   master_session = setup_master_session(argv[1]);
 
+
+  printf("SETTING UP MQ\n");
+  if ((key = ftok("server.c", 'B')) == -1) {
+      perror("ftok");          exit(1);
+  }
+
+  if ((msq_id = msgget(key, 0644 | IPC_CREAT)) == -1) {
+      perror("msgget");          exit(1);
+  }
+  send_mq(DATASE_CORRECT_NOTIFICATION,2); //TODO: cambiar el 2
+
+
   while (1) {
     printf("[SV]: Accepting...\n");
     session = accept_client(master_session);
     printf("[SV]: ACCEPTED!\n");
+    send_mq(CLIENT_ACCEPTED,2); //TODO: VER LA PRIOIDAD (DEBE SER > 0 )
     create_thread(argv[2], session);
   }
 }
@@ -100,6 +126,7 @@ void * run_thread(void * p) {
 
   if (valid == 0) {
     printf("Client disconnected\n");
+    send_mq(CLIENT_DISCONNECTED,2);
   }
   else if (valid == -1) {
     printf("Failed to send response\n");
@@ -110,28 +137,39 @@ void * run_thread(void * p) {
 
 // TODO: CAMBIAR LOS BUFSIZE
 int sv_tweet(void * p, char * user, char * msg) {
+  char mq_msg[MAX_NOTIFICATION];
   char buffer[BUFSIZE], res[BUFSIZE];
   printf("RECEIVED SV_TWEET_WITH \nuser:%s \nmsg:%s\n", user, msg);
   query_insert(buffer, user, msg);
   send_query(p, buffer, res);
 
+  sprintf(mq_msg,TWEET_NOTIFICATION,user,res,msg);
+  send_mq(mq_msg,2); //TODO: cambiar el 2
+
   return atoi(res);
 }
 
 t_tweet * sv_refresh(void * p, int last_id, char res[]) {
-  char buffer[BUFSIZE];
+  char buffer[BUFSIZE],mq_msg[MAX_NOTIFICATION];
   printf("RECEIVED SV_REFRESH WITH \nid:%d\n", last_id);
   query_refresh(buffer, last_id);
   send_query(p, buffer, res);
+
+  sprintf(mq_msg,REFRESH_NOTIFICATION);
+  send_mq(mq_msg,2); //TODO: cambiar el 2
 
   return NULL;
 }
 
 int sv_like(void * p, int id) {
   char buffer[BUFSIZE], res[BUFSIZE];
+  char mq_msg[MAX_NOTIFICATION];
   printf("RECEIVED SV_LIKE WITH \nid:%d\n", id);
   query_like(buffer, id);
   send_query(p, buffer, res);
+
+  sprintf(mq_msg,LIKE_NOTIFICATION,id);
+  send_mq(mq_msg,2); //TODO:cambiar el 2
 
   return atoi(res);
 }
@@ -150,4 +188,20 @@ void send_query(t_session_data * data, const char *sql, char result[]) {
 
   get_response_msg(res, result);
   printf("%s\n", result);
+}
+
+int send_mq(char * msg, int priority){
+  queue_buffer * buf = malloc(sizeof(*buf));
+  int len = strlen(msg);
+  buf->mtype = priority;
+
+  strcpy(buf->mtext, msg);
+  if (msgsnd(msq_id, buf, len+1, 0) == -1) {
+      perror("ERROR ON MSGSND...");
+      printf ("%d, %d, %s, %d\n", msq_id, buf->mtype, buf->mtext, len);
+      free(buf);
+      return 1;
+  }
+  free(buf);
+  return 0;
 }
